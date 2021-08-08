a401ded7425da4125d9513ffcb62079ff297b976
hungrykirby <katsuki8hanawa6@gmail.com>
2021-08-08 23:31:44 +0900

    最低限動くのでmasterにpush！！

---
+++ app.py
@@ -0,0 +1,31 @@
+import glob
+import os
+import datetime
+import shutil
+
+NINTENDO_FOLDER_NAME = 'Nintendo'
+OUTPUT = 'Output'
+
+def mkdir_with_timestamp_in_output():
+    dt_now = datetime.datetime.now()
+    dirname_with_timestamp = dt_now.strftime('%Y%m%d%H%M%S')
+    output_dir = os.path.join(OUTPUT, dirname_with_timestamp)
+    os.mkdir(output_dir)
+    return output_dir
+
+def main():
+    # テスト時にめんどくさいのでOutputというディレクトリの中に実行時間のディレクトリを作成してその中にコピーする
+    output_dir = mkdir_with_timestamp_in_output()
+    # Nintendo / Album / #{year} / #{month} / #{date} というディレクトリ構造のはずなのでこれでフォルダがすべて取得できる
+    jpg_images_path = os.path.join(NINTENDO_FOLDER_NAME, 'Album', '*', '*', '*')
+    # jpg or mp4ファイルのはずだが拡張子を限定していない。
+    files = glob.glob(jpg_images_path + '/*')
+    for f in files:
+        # 拡張子を含めたファイル名だけを取得
+        # コピー後もファイル名を引き継ぎたいのでこうしている
+        basename = os.path.basename(f)
+        output_file = os.path.join(output_dir, basename)
+        shutil.copy2(f, output_file)
+
+if __name__ == "__main__":
+    main()
