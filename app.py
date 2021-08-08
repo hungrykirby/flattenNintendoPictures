d81a7e707b1ce8a371468dd3cfadc5b585c6bc56
hungrykirby <katsuki8hanawa6@gmail.com>
2021-08-08 23:53:40 +0900

    readme作ったからmaster push！！！

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import os
 import datetime
 import shutil
 
+# Nintendoという名前のフォルダを中身ごとカレントディレクトリにコピーしている前提
 NINTENDO_FOLDER_NAME = 'Nintendo'
+# 実行前にカレントディレクトリにOutputというフォルダを作って実行
 OUTPUT = 'Output'
 
 def mkdir_with_timestamp_in_output():
@@ -14,7 +16,7 @@ def mkdir_with_timestamp_in_output():
     return output_dir
 
 def main():
-    # テスト時にめんどくさいのでOutputというディレクトリの中に実行時間のディレクトリを作成してその中にコピーする
+    # テスト時にめんどくさいのでOutputというディレクトリの中に実行時間のフォルダを作成してその中にコピーする
     output_dir = mkdir_with_timestamp_in_output()
     # Nintendo / Album / #{year} / #{month} / #{date} というディレクトリ構造のはずなのでこれでフォルダがすべて取得できる
     jpg_images_path = os.path.join(NINTENDO_FOLDER_NAME, 'Album', '*', '*', '*')
@@ -25,6 +27,9 @@ def main():
         # コピー後もファイル名を引き継ぎたいのでこうしている
         basename = os.path.basename(f)
         output_file = os.path.join(output_dir, basename)
+        # copy2関数：ファイルのメタデータ（作成時間、変更時間、その他の情報）も可能な限りコピーしようとすることを除けばcopy関数と同様
+        # メタデータもコピーしておく
+        # どこまで厳密にコピーしてくれるのかは知らない
         shutil.copy2(f, output_file)
 
 if __name__ == "__main__":
